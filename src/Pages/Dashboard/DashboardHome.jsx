7af28abdb7fc0a12c95ca351367b8da02d838e1b
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-11-09 12:55:25 +0600

    Additional Design for Dashboard from niaz

--- src/Pages/Dashboard/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome.jsx
@@ -1,87 +1,63 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context/AuthProvider";
 import axios from "axios";
+import Showing_Dashboard_Info from "./Showing_Dashboard_Info";
 
 const DashboardHome = () => {
   const { user } = useContext(AuthContext);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    axios.get(`http://localhost:3000/`);
+    axios
+      .get(`http://localhost:3000/dashboard-info/${user.email}`)
+      .then((res) => {
+        // console.log(res.data);
+        setData(res.data);
+      });
   }, []);
 
-  
-
-
   return (
-    <div className="flex flex-col fixed border min-h-screen my-auto">
-      <div className="">
-        <svg
-          width="1017.000000"
-          height="140.000000"
-          viewBox="0 0 1017 140"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-        >
-          <desc>Created with Pixso.</desc>
-          <defs />
-          <path
-            id="Mask"
-            d="M20 0L997 0C1008.05 0 1017 8.95 1017 20L1017 140L0 140L0 20C0 8.95 8.95 0 20 0Z"
-            fill="#428777"
-            fill-opacity="1.000000"
-            fill-rule="evenodd"
+    <div className="grid grid-cols-3 border border-red-600 min-h-screen">
+      <div className="col-span-2 border border-yellow-800 m-2  relative">
+        {/* Background SVG */}
+        <div className="relative">
+          <svg
+            width="full"
+            height="140px"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M20 0L997 0C1008.05 0 1017 8.95 1017 20L1017 140L0 140L0 20C0 8.95 8.95 0 20 0Z"
+              fill="#428777"
+            />
+          </svg>
+        </div>
+
+        {/* Profile image and name */}
+        <div className="relative -mt-12 ml-2 flex items-center rounded-full">
+          <img
+            className=" rounded-full"
+            src={data.photo_url}
+            alt="Profile"
           />
-        </svg>
+          <div className="ml-4">
+            <h1 className="text-5xl font-bold text-black">{data.name}</h1>
+            <p className="text-3xl font-bold text-green-800">{data.role}</p>
+            <p className="text-lg font-bold text-black">{data.email}</p>
+          </div>
+        </div>
+
+        {/* Additional content below */}
+        <div className="mt-4 p-4 border min-h-screen">
+          <h1 className="text-xl font-semibold">jvbdsvbsud</h1>
+        </div>
       </div>
-      <div className="absolute mt-10 ml-4">
-        <svg
-          width="200.000000"
-          height="200.000015"
-          viewBox="0 0 200 200"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-        >
-          <desc>Created with Pixso.</desc>
-          <defs />
-          <rect
-            id="placeholder"
-            y="0.000010"
-            rx="100.000000"
-            width="200.000000"
-            height="200.000000"
-            fill="#C1BBEB"
-            fill-opacity="1.000000"
-          />
-          <rect
-            id="placeholder"
-            x="4.000000"
-            y="4.000010"
-            rx="96.000000"
-            width="192.000000"
-            height="192.000000"
-            stroke="#FFFFFF"
-            stroke-opacity="1.000000"
-            stroke-width="8.000000"
-          />
-        </svg>
-      </div>
-      <div className="pt-24"></div>
-      <div className="p-6">
-        <h1 className="text-3xl font-bold">Walter White</h1>
-        <p className="font-bold">Student</p>
-      </div>
-      <div className="grid grid-cols-4 pl-32">
-        <h1>Parents</h1>
-        <h1>Address</h1>
-        <h1>Phone</h1>
-        <h1>Email</h1>
-      </div>
-      <div className=" grid grid-cols-4">
-        <p>Carl Max</p>
-        <p>Dhoom 5</p>
-        <p>43857325892348</p>
-        <p>kiss@gmail.com</p>
+
+      <div className="col-span-1 border border-green-700 m-4">
+        <div>
+          <h1>Event Comming</h1>
+        </div>
       </div>
     </div>
   );
---
+++ src/Pages/Dashboard/Showing_Dashboard_Info.jsx
@@ -0,0 +1,12 @@
+
+
+const Showing_Dashboard_Info = ({info}) => {
+    const {name} = info;
+    return (
+        <div>
+            {name}
+        </div>
+    );
+};
+
+export default Showing_Dashboard_Info;
